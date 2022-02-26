7ad2c67108f0ce918c1fee8dd9cc58abaf96faef
Anthony Ronda <anthonyronda@gmail.com>
2022-02-26 17:16:02 -0500

    fix: use asterisk to pass all args correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sh -c "cd $1 && sanity install && SANITY_AUTH_TOKEN=$SANITY_AUTH_TOKEN sanity ${@:2:99}"
+sh -c "cd $1 && sanity install && SANITY_AUTH_TOKEN=$SANITY_AUTH_TOKEN sanity ${*:2:99}"
